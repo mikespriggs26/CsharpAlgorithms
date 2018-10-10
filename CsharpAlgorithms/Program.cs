e52b82016928c668c3a009285ce20c96e47fdbf5
mikespriggs26 <mikespriggs@msn.com>
2018-10-09 20:08:54 -0400

    Updates

--- CsharpAlgorithms/Program.cs
+++ CsharpAlgorithms/Program.cs
@@ -24,7 +24,27 @@ namespace CsharpAlgorithms
 
             //sort an array in ascending order
 
+            //Is prime
+            bool isPrime = true;
+            Console.WriteLine("Prime Numbers : ");
+            for (int i = 2; i <= 100; i++)
+            {
+                for (int j = 2; j <= 100; j++)
+                {
 
+                    if (i != j && i % j == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+
+                }
+                if (isPrime)
+                {
+                    Console.Write("\t" + i);
+                }
+                isPrime = true;
+            }
 
             //Is prime -- NOT DONE
             //bool isPrime = false;
